eb6554d65548ea831e06372355b4c5ead96da963
Guyutongxue <guyutongxue@163.com>
2025-12-15 18:58:21 +0800

    fix water-mark breaking nth-child

--- src/components/renderer/Watermark.tsx
+++ src/components/renderer/Watermark.tsx
@@ -12,34 +12,36 @@ export const Watermark = (props: WatermarkProps) => {
   return (
     <Show when={sanitized()}>
       {(text) => (
-        <svg class="watermark-svg" aria-hidden="true">
-          <defs>
-            <pattern
-              id={patternId}
-              patternUnits="userSpaceOnUse"
-              width="856"
-              height="428"
-            >
-              <g transform="translate(428 214) rotate(-20)">
-                <text
-                  text-anchor="middle"
-                  dominant-baseline="middle"
-                  font-family="HYWH, sans-serif"
-                  font-style="italic"
-                  font-size="170"
-                  fill="#716864"
-                  fill-opacity="0.03"
-                  letter-spacing="-1rem"
-                >
-                  {text()}
-                </text>
-              </g>
-            </pattern>
-          </defs>
-          <rect width="100%" height="100%" fill={`url(#${patternId})`} />
-        </svg>
+        <>
+          <svg class="watermark-svg" aria-hidden="true">
+            <defs>
+              <pattern
+                id={patternId}
+                patternUnits="userSpaceOnUse"
+                width="856"
+                height="428"
+              >
+                <g transform="translate(428 214) rotate(-20)">
+                  <text
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    font-family="HYWH, sans-serif"
+                    font-style="italic"
+                    font-size="170"
+                    fill="#716864"
+                    fill-opacity="0.03"
+                    letter-spacing="-1rem"
+                  >
+                    {text()}
+                  </text>
+                </g>
+              </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill={`url(#${patternId})`} />
+          </svg>
+          <hr class="invisible h-0 w-0" />
+        </>
       )}
     </Show>
   );
 };
-
